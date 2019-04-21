c8a079cc649ea1a205a657d52940dcea976cdf4d
IgorChavychalov <Protor_fenix@mail.ru>
2019-04-21 20:37:02 +0300

    ДЗ-2.2:     -OAuth2: настройки     -google: sign in

--- authapp/models.py
+++ authapp/models.py
@@ -11,7 +11,7 @@ def activation_key_expired():
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveSmallIntegerField(verbose_name='возраст')
+    age = models.PositiveSmallIntegerField(verbose_name='возраст', default=18)
     # активация по e-mail
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(default=activation_key_expired)
