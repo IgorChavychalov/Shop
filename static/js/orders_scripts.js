d46b929f4ca9441bfd4cc40c6daf4e19409c639b
IgorChavychalov <Protor_fenix@mail.ru>
2019-05-05 23:05:03 +0300

    ДЗ-2.4:     -удаление возвращение продуктов в базу     -через сигналы     -quantity_arr, var price_arr     -обновление стоимости в ордерах     -кнопки удаление и добавление продуктов

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -1,8 +1,8 @@
 var _quantity, _price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
 var quantity_arr = [];
 var price_arr = [];
-var $orderForm;
-var order_total_quantity, order_total_cost, TOTAL_FORMS;
+var $orderForm, $orderTotalQuantity, $orderTotalCost;
+var order_total_quantity, order_total_cost, TotalForms;
 
 
 function orderSummaryUpdate(orderitem_price, delta_quantity) {
@@ -12,18 +12,27 @@ function orderSummaryUpdate(orderitem_price, delta_quantity) {
     order_total_quantity = order_total_quantity + delta_quantity;
 
     $('.order_total_cost').html(order_total_cost.toString());
-    $('.order_total_quantity').html(order_total_quantity.toString());
+    $orderTotalQuantity.html(order_total_quantity.toString());
+}
+
+function deleteOrderItem(row) {
+    var target_name= row[0].querySelector('input[type="number"]').name;
+    orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+    delta_quantity = -quantity_arr[orderitem_num];
+    orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
 }
 
 
 window.onload = function () {
     $orderForm = $('.order_form');
-    TOTAL_FORMS = parseInt($('input[name="orderitems-TOTAL_FORMS"]').val());
+    $orderTotalQuantity = $('.order_total_quantity');
+    $orderTotalCost = $('.order_total_cost');
+    TotalForms = parseInt($('input[name="orderitems-TotalForms"]').val());
 
-    order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
-    order_total_cost = parseFloat($('.order_total_cost').text().replace(',', '.')) || 0;
+    order_total_quantity = parseInt($orderTotalQuantity.text()) || 0;
+    order_total_cost = parseFloat($orderTotalCost.text().replace(',', '.')) || 0;
 
-    for (var i=0; i < TOTAL_FORMS; i++) {
+    for (var i=0; i < TotalForms; i++) {
         _quantity = parseInt($('input[name="orderitems-' + i + '-quantity"]').val());
         _price = parseFloat($('.orderitems-' + i + '-price').text().replace(',', '.'));
         quantity_arr[i] = _quantity;
@@ -31,12 +40,12 @@ window.onload = function () {
     }
 
     if (!order_total_quantity) {
-        for (var i=0; i < TOTAL_FORMS; i++) {
+        for (var i=0; i < TotalForms; i++) {
             order_total_quantity += quantity_arr[i];
             order_total_cost += quantity_arr[i] * price_arr[i];
         }
-        $('.order_total_quantity').html(order_total_quantity.toString());
-        $('.order_total_cost').html(Number(order_total_cost.toFixed(2)).toString());
+        $orderTotalQuantity.html(order_total_quantity.toString());
+        $orderTotalCost.html(Number(order_total_cost.toFixed(2)).toString());
     }
 
     $orderForm.on('change', 'input[type="number"]', function (event) {
@@ -59,5 +68,12 @@ window.onload = function () {
             delta_quantity = quantity_arr[orderitem_num];
         }
         orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    });
+
+    $('.formset_row').formset({
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        prefix: 'orderitems',
+        removed: deleteOrderItem
     });
 };
